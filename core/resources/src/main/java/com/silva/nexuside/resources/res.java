6aaf6ce9396f47758cad7d47424e4a834b3147e0
Aquiles Trindade <devsuay@gmail.com>
2024-10-13 20:48:11 -0300

    Update res.java

--- core/resources/src/main/java/com/silva/nexuside/resources/res.java
+++ core/resources/src/main/java/com/silva/nexuside/resources/res.java
@@ -1,29 +1,21 @@
 package com.silva.nexuside.resources;
 
-import androidx.annotation.DrawableRes;
-import androidx.annotation.StringRes;
-import androidx.annotation.ColorRes;
-import androidx.annotation.StyleRes;
-import androidx.annotation.IdRes;
-import androidx.annotation.MenuRes;
-import androidx.annotation.LayoutRes;
-
 import com.silva.nexuside.resources.R;
 
 public class res {
     
-    public static final @DrawableRes int Drawables = R.drawable;
+    public static final R Drawables = R.drawable;
     
-    public static final @StringRes int Strings = R.string;
+    public static final R Strings = R.string;
     
-    public static final @ColorRes int Colors = R.color;
+    public static final R Colors = R.color;
     
-    public static final @StyleRes int Styles = R.style;
+    public static final R Styles = R.style;
     
-    public static final @IdRes int Ids = R.id;
+    public static final R Ids = R.id;
     
-    public static final @MenuRes int Menus = R.menu;
+    public static final R Menus = R.menu;
     
-    public static final @LayoutRes int Layouts = R.layout;
+    public static final R Layouts = R.layout;
     
 }
